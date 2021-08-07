bee11b3424ee4da567e6c93c9ad506244eae68a8
ahmedelsayedsaleh <ahmed0elsayed0saleh@gmail.com>
2021-08-07 03:22:10 +0200

    edit form contact with netlify

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -15,7 +15,7 @@ function Contact() {
             </div>
             <div className="right">
                 <h2>Contact.</h2>
-                <form onSubmit={handleSubmit} name="contact" method="POST" data-netlify="true" netlify >
+                <form onSubmit={handleSubmit} name="contact" method="POST" data-netlify="true">
                     <input type="text" placeholder="Name" name="name" required/>
                     <input type="email" placeholder="Email" name="email" required/>
                     <textarea placeholder="Message" name="message" required/>
